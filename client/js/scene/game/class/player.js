1033d21cd0b24fbf3d5eab40093d2490fd998c20
Philippe Haerens <haerphi@gmail.com>
2019-11-28 13:33:41 +0100

    toClass done

--- client/js/scene/game/class/player.js
+++ client/js/scene/game/class/player.js
@@ -1,7 +1,7 @@
 class Player {
-  constructor(x, y, sprite) {
+  constructor(x, y, sprite, gameObj) {
     //PLAYER
-    this.player = this.physics.add.sprite(x, y, sprite);
+    this.player = gameObj.physics.add.sprite(x, y, sprite);
 
     //the player won't go out of the world
     this.player.setCollideWorldBounds(true);
@@ -14,21 +14,93 @@ class Player {
 
     //CAMERA
     // making the camera following the player
-    this.cameras.main.startFollow(this.player);
+    gameObj.cameras.main.startFollow(this.player);
 
     //making sure that the camera will not get out of the world
-    this.cameras.main.setBounds(0, 0, this.gameWidth, this.gameHeight);
+    gameObj.cameras.main.setBounds(0, 0, gameObj.gameWidth, gameObj.gameHeight);
 
     //UI PART
     // speed display fixed to camera
-    this.displaySpeed = this.add.text(16, 16, `speed : ${this.vel}`);
+    this.displaySpeed = gameObj.add.text(16, 16, `speed : ${this.vel}`);
     this.displaySpeed.setScrollFactor(0); // this line said that the text will not scroll and will be fixed on the screen
 
-    this.displayRotation = this.add.text(
+    this.displayRotation = gameObj.add.text(
       16,
       64,
       `angle : ${this.player.angle - 90}`
     );
     this.displayRotation.setScrollFactor(0); // this line said that the text will not scroll and will be fixed on the screen
+
+    if (true) {
+      // define the key player key
+      this.keyZ = gameObj.input.keyboard.addKey(
+        Phaser.Input.Keyboard.KeyCodes.Z
+      );
+      this.keyQ = gameObj.input.keyboard.addKey(
+        Phaser.Input.Keyboard.KeyCodes.Q
+      );
+      this.keyS = gameObj.input.keyboard.addKey(
+        Phaser.Input.Keyboard.KeyCodes.S
+      );
+      this.keyD = gameObj.input.keyboard.addKey(
+        Phaser.Input.Keyboard.KeyCodes.D
+      );
+    }
+  }
+
+  move() {
+    if (this.keyZ.isDown) {
+      this.accelerate();
+    }
+    if (this.keyS.isDown) {
+      this.decelerate();
+    }
+    if (this.keyQ.isDown) {
+      this.turnLeft();
+    }
+    if (this.keyD.isDown) {
+      this.turnRight();
+    }
+  }
+
+  accelerate() {
+    if (this.vel < 400) {
+      this.vel += 3;
+    } else {
+    }
+    this.player.setVelocity(
+      Math.sin(this.player.rotation) * this.vel,
+      Math.cos(this.player.rotation) * -this.vel
+    ); // 1
+    this.displaySpeed.setText(`speed : ${this.vel}`);
+  }
+
+  decelerate() {
+    if (this.vel > 0) {
+      this.vel -= 3;
+    }
+    this.player.setVelocity(
+      Math.sin(this.player.rotation) * this.vel,
+      Math.cos(this.player.rotation) * -this.vel
+    );
+    this.displaySpeed.setText(`speed : ${this.vel}`);
+  }
+
+  turnLeft() {
+    this.player.setVelocity(
+      Math.sin(this.player.rotation) * this.vel,
+      Math.cos(this.player.rotation) * -this.vel
+    );
+    this.player.angle -= 6;
+    this.displayRotation.setText(`angle : ${this.player.angle.toFixed(0)}`);
+  }
+
+  turnRight() {
+    this.player.setVelocity(
+      Math.sin(this.player.rotation) * this.vel,
+      Math.cos(this.player.rotation) * -this.vel
+    );
+    this.player.angle += 6;
+    this.displayRotation.setText(`angle : ${this.player.angle.toFixed(0)}`);
   }
 }
--- client/js/scene/game/sceneGame.js
+++ client/js/scene/game/sceneGame.js
@@ -38,76 +38,10 @@ class GameScene extends Phaser.Scene {
       .setScrollFactor(3);
 
     // create the player
-    this.player = this.physics.add.sprite(50, 300, "playerShip");
-    this.player.setCollideWorldBounds(true);
-
-    // get the player in the correct position (thank to the sprite.)
-    this.player.angle = 90;
-
-    // making the camera following the player
-    this.cameras.main.startFollow(this.player);
-
-    //making sure that the camera will not get out of the world
-    this.cameras.main.setBounds(0, 0, this.gameWidth, this.gameHeight);
-
-    //define the player velocity that will be used by setVelocity
-    this.vel = 0;
-
-    // speed display fixed to camera
-    this.displaySpeed = this.add.text(16, 16, `speed : ${this.vel}`);
-    this.displaySpeed.setScrollFactor(0); // this line said that the text will not scroll and will be fixed on the screen
-
-    this.displayRotation = this.add.text(
-      16,
-      64,
-      `angle : ${this.player.angle - 90}`
-    );
-    this.displayRotation.setScrollFactor(0); // this line said that the text will not scroll and will be fixed on the screen
-
-    // define the key player key
-    this.keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-    this.keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
-    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-    this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    this.player = new Player(50, 300, "playerShip", this);
   }
 
   update() {
-    if (this.keyZ.isDown) {
-      if (this.vel < 400) {
-        this.vel += 3;
-      } else {
-      }
-      this.player.setVelocity(
-        Math.sin(this.player.rotation) * this.vel,
-        Math.cos(this.player.rotation) * -this.vel
-      ); // 1
-      this.displaySpeed.setText(`speed : ${this.vel}`);
-    }
-    if (this.keyS.isDown) {
-      if (this.vel > 0) {
-        this.vel -= 3;
-      }
-      this.player.setVelocity(
-        Math.sin(this.player.rotation) * this.vel,
-        Math.cos(this.player.rotation) * -this.vel
-      );
-      this.displaySpeed.setText(`speed : ${this.vel}`);
-    }
-    if (this.keyQ.isDown) {
-      this.player.setVelocity(
-        Math.sin(this.player.rotation) * this.vel,
-        Math.cos(this.player.rotation) * -this.vel
-      );
-      this.player.angle -= 6;
-      this.displayRotation.setText(`angle : ${this.player.angle.toFixed(0)}`);
-    }
-    if (this.keyD.isDown) {
-      this.player.setVelocity(
-        Math.sin(this.player.rotation) * this.vel,
-        Math.cos(this.player.rotation) * -this.vel
-      );
-      this.player.angle += 6;
-      this.displayRotation.setText(`angle : ${this.player.angle.toFixed(0)}`);
-    }
+    this.player.move();
   }
 }
